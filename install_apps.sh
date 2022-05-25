d7a0ae8f986c3bd7dedf8e89c02d86618faf7881
Emmanuel Hayford <hi@manny.codes>
2022-05-25 16:30:50 +0200

    Update install script

--- install_apps.sh
+++ install_apps.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-
 # Install Homebrew
-
 echo "Installing brew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing brew cask..."
 brew tap homebrew/cask
 
-
 # Development
-
 brew install --cask iterm2
 brew install --cask docker
 brew install --cask postman
@@ -23,22 +19,18 @@ brew install --cask visual-studio-code
 brew install --cask github
 
 # Utilities
-
 brew install --cask vlc
 
 # Browsers
-
 brew install --cask google-chrome
 brew install --cask firefox
 
 # Communication
-
 brew install --cask slack
 brew install --cask whatsapp
 brew install --cask zoomus
 
 # Entertainment
-
 brew install --cask spotify
 
 # Internet Speed Test
@@ -51,3 +43,5 @@ brew install task
 
 brew install htop
 brew install tree
+
+brew install mas
