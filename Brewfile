01b59fc9210a0c43fb7f2358e85381c11885244b
Emmanuel Hayford <hi@manny.codes>
2022-05-25 16:10:12 +0200

    Add Brewfile

---
+++ Brewfile
@@ -0,0 +1,39 @@
+cask "iterm2"
+cask "docker"
+cask "postman"
+brew "node"
+brew "yarn"
+brew "nvm"
+brew "awscli"
+cask "visual-studio-code"
+cask "github"
+
+# Utilities
+
+cask "vlc"
+
+# Browsers
+
+cask "google-chrome"
+cask "firefox"
+
+# Communication
+
+cask "slack"
+cask "whatsapp"
+cask "zoom"
+
+# Entertainment
+
+cask "spotify"
+
+# Internet Speed Test
+tap "teamookla/speedtest"
+brew "speedtest"
+
+# Learn more about this later
+# https://taskwarrior.org/docs/
+brew "task"
+
+brew "htop"
+brew "tree"
--- install_apps.sh
+++ install_apps.sh
@@ -1,4 +1,14 @@
-#/bin/sh
+#!/bin/sh
+
+
+# Install Homebrew
+
+echo "Installing brew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing brew cask..."
+brew tap homebrew/cask
+
 
 # Development
 
@@ -7,6 +17,7 @@ brew install --cask docker
 brew install --cask postman
 brew install node
 brew install yarn
+brew install nvm
 brew install awscli
 brew install --cask visual-studio-code
 brew install --cask github
@@ -29,3 +40,14 @@ brew install --cask zoomus
 # Entertainment
 
 brew install --cask spotify
+
+# Internet Speed Test
+brew tap teamookla/speedtest
+brew install speedtest --force
+
+# Learn more about this later
+# https://taskwarrior.org/docs/
+brew install task
+
+brew install htop
+brew install tree
